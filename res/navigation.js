b63c59311fa86a743a77feaf73d40b59a0d5be74
animal <no>
2022-02-02 11:35:48 +0000

    fix #3 for links

--- res/navigation.js
+++ res/navigation.js
@@ -9,46 +9,46 @@ $('#navbar').html(function () {
   <h3 class="nav-title">Movecraft</h3>
   <ul>
   <!-- //* movecraft controls -->
-  <li><a id="movecraft-controls" href="/movecraft/controls.html">Signs / Controls</a>
+  <li><a id="movecraft-controls" href="movecraft/controls.html">Signs / Controls</a>
   <ul id="movecraft-controls-ul" class="dropdown">
-  <li><a href="/movecraft/controls.html#compass">Compass control</a></li>
-  <li><a href="/movecraft/controls.html#gears">Gear control</a></li>
-  <li><a href="/movecraft/controls.html#direct">Direct control</a></li>
-  <li><a href="/movecraft/controls.html#pilot">Pilot signs</a></li>
-  <li><a href="/movecraft/controls.html#helm">Helm sign</a></li>
-  <li><a href="/movecraft/controls.html#cruise">Cruise signs</a></li>
-  <li><a href="/movecraft/controls.html#release">Release sign</a></li>
-  <li><a href="/movecraft/controls.html#secure">Pilot: sign</a></li>
+  <li><a href="movecraft/controls.html#compass">Compass control</a></li>
+  <li><a href="movecraft/controls.html#gears">Gear control</a></li>
+  <li><a href="movecraft/controls.html#direct">Direct control</a></li>
+  <li><a href="movecraft/controls.html#pilot">Pilot signs</a></li>
+  <li><a href="movecraft/controls.html#helm">Helm sign</a></li>
+  <li><a href="movecraft/controls.html#cruise">Cruise signs</a></li>
+  <li><a href="movecraft/controls.html#release">Release sign</a></li>
+  <li><a href="movecraft/controls.html#secure">Pilot: sign</a></li>
   </ul>
   </li>
   <!-- //* movecraft craftypes -->
-  <li><a id="movecraft-crafts" href="/movecraft/crafts.html">Craft types</a>
+  <li><a id="movecraft-crafts" href="movecraft/crafts.html">Craft types</a>
   <ul id="movecraft-crafts-ul" class="dropdown">
-  <li><a href="/movecraft/crafts.html#starfighter">Starfighter</a></li>
-  <li><a href="/movecraft/crafts.html#cargoshuttle">Cargoshuttle</a></li>
-  <li><a href="/movecraft/crafts.html#starship">Starship</a></li>
-  <li><a href="/movecraft/crafts.html#freighter">Freighter</a></li>
-  <li><a href="/movecraft/crafts.html#dreadnaught">Dreadnaught</a></li>
-  <li><a href="/movecraft/crafts.html#station">Station</a></li>
-  <li><a href="/movecraft/crafts.html#startitan">StarTitan</a></li>
-  <li><a href="/movecraft/crafts.html#airship">Airship</a></li>
-  <li><a href="/movecraft/crafts.html#plane">Plane</a></li>
-  <li><a href="/movecraft/crafts.html#tank">Tank</a></li>
-  <li><a href="/movecraft/crafts.html#train">Train</a></li>
-  <li><a href="/movecraft/crafts.html#landtitan">LandTitan</a></li>
-  <li><a href="/movecraft/crafts.html#ship">Ship</a></li>
-  <li><a href="/movecraft/crafts.html#skiff">Skiff</a></li>
-  <li><a href="/movecraft/crafts.html#submarine">Submarine</a></li>
+  <li><a href="movecraft/crafts.html#starfighter">Starfighter</a></li>
+  <li><a href="movecraft/crafts.html#cargoshuttle">Cargoshuttle</a></li>
+  <li><a href="movecraft/crafts.html#starship">Starship</a></li>
+  <li><a href="movecraft/crafts.html#freighter">Freighter</a></li>
+  <li><a href="movecraft/crafts.html#dreadnaught">Dreadnaught</a></li>
+  <li><a href="movecraft/crafts.html#station">Station</a></li>
+  <li><a href="movecraft/crafts.html#startitan">StarTitan</a></li>
+  <li><a href="movecraft/crafts.html#airship">Airship</a></li>
+  <li><a href="movecraft/crafts.html#plane">Plane</a></li>
+  <li><a href="movecraft/crafts.html#tank">Tank</a></li>
+  <li><a href="movecraft/crafts.html#train">Train</a></li>
+  <li><a href="movecraft/crafts.html#landtitan">LandTitan</a></li>
+  <li><a href="movecraft/crafts.html#ship">Ship</a></li>
+  <li><a href="movecraft/crafts.html#skiff">Skiff</a></li>
+  <li><a href="movecraft/crafts.html#submarine">Submarine</a></li>
   </ul>
   <!-- //* movecraft blocks -->
-  <li><a id="movecraft-blocks" href="/movecraft/blocks.html">Valid blocks</a></li>
+  <li><a id="movecraft-blocks" href="movecraft/blocks.html">Valid blocks</a></li>
   </ul>
   <h3 class="nav-title">Planets</h3>
   <ul>
   <!-- //* planet resources -->
-  <li><a id="planets-resources" href="/planets/resources.html">Resources</a></li>
+  <li><a id="planets-resources" href="planets/resources.html">Resources</a></li>
   <!-- //* interplanetary travel -->
-  <li><a id="planets-travel" href="/planets/travel.html">Interplanetary travel</a></li>
+  <li><a id="planets-travel" href="planets/travel.html">Interplanetary travel</a></li>
   </ul>
   </nav>
   <div style="display: flex;">
@@ -59,25 +59,25 @@ $('#navbar').html(function () {
   <h3 class="nav-title">Resonant</h3>
   <ul>
   <!-- //* resonant management -->
-  <li><a id="resonant-management" href="/resonant/management.html">Generation / Storage</a></li>
+  <li><a id="resonant-management" href="resonant/management.html">Generation / Storage</a></li>
   <!-- //* resonant machinery -->
-  <li><a id="resonant-machines" href="/resonant/machines.html">Machines</a></li>
+  <li><a id="resonant-machines" href="resonant/machines.html">Machines</a></li>
   </ul>
   <h3 class="nav-title">Multiblock Structures</h3>
   <ul>
   <!-- //* multiblock machinery -->
-  <li><a id="multiblock-machinery" href="/multiblock/machinery.html">Machinery</a></li>
+  <li><a id="multiblock-machinery" href="multiblock/machinery.html">Machinery</a></li>
   <!-- //* multiblock automation -->
-  <li><a id="multiblock-automation" href="/multiblock/automation.html">Automation</a></li>
+  <li><a id="multiblock-automation" href="multiblock/automation.html">Automation</a></li>
   <!-- //* multiblock weapons -->
-  <li><a id="multiblock-weapons" href="/multiblock/weapons.html">Weapons</a></li>
+  <li><a id="multiblock-weapons" href="multiblock/weapons.html">Weapons</a></li>
   </ul>
   <h3 class="nav-title">Guides</h3>
   <ul>
   <!-- //* leaving earth guide -->
-  <li><a id="guides-leaving-earth" href="/guides/leaving-earth.html">Getting off earth</a></li>
+  <li><a id="guides-leaving-earth" href="guides/leaving-earth.html">Getting off earth</a></li>
   <!-- //* moneymaking guide -->
-  <li><a id="guides-moneymaking" href="/guides/moneymaking.html">Moneymaking</a></li>
+  <li><a id="guides-moneymaking" href="guides/moneymaking.html">Moneymaking</a></li>
   </ul>
 */
 })
@@ -86,7 +86,7 @@ $('#navbar').html(function () {
 const $disclaimer = $('#disclaimer');
 
 $disclaimer.html(
-`
+  `
   <h3>Please note that this site was designed on a desktop for desktops, and while I have tried to make it respond to smaller screen sizes it may render oddly on mobile devices, you have been warned! (tap to dismiss)
   </h3>
 `
