ed1b754d0d4609838990586ac8f2e1af776e04bc
animal <no>
2022-02-08 12:23:03 +0000

    create and finish resonant/basics.html entry.

--- res/navigation.js
+++ res/navigation.js
@@ -72,35 +72,37 @@ $('#navbar').html(function () {
   <!-- //* interplanetary travel -->
   <li><a id="planets-travel" href="../planets/travel.html">Interplanetary travel</a></li>
   </ul>
+  <h3 class="nav-title">Resonant</h3>
+  <ul>
+  <!-- //* resonant basics -->
+  <strike>
+  <li><a id="resonant-basics" href="../resonant/basics.html">Basics</a></li>
+  </strike>
   </nav>
   <div style="display: flex;">
   <span><img src="../images/icons/author.png" style="margin: 16px;"></span><p style="color: var(--signatureColour)">Written by <br>animal</p>
   </div>
   `;
   /*
-  <h3 class="nav-title">Resonant</h3>
-  <ul>
-  //* resonant basics
-  <li><a id="resonant-basics" href="../resonant/basics.html">Basics</a></li>
-  //* resonant management
+  <!-- //* resonant management -->
   <li><a id="resonant-management" href="../resonant/management.html">Generation / Storage</a></li>
-  //* resonant machinery
+  <!-- //* resonant machinery -->
   <li><a id="resonant-machines" href="../resonant/machines.html">Machines</a></li>
   </ul>
   <h3 class="nav-title">Multiblock Structures</h3>
   <ul>
-  //* multiblock machinery
+  <!-- //* multiblock machinery -->
   <li><a id="multiblock-machinery" href="../multiblock/machinery.html">Machinery</a></li>
-  //* multiblock automation
+  <!-- //* multiblock automation -->
   <li><a id="multiblock-automation" href="../multiblock/automation.html">Automation</a></li>
-  //* multiblock weapons
+  <!-- //* multiblock weapons -->
   <li><a id="multiblock-weapons" href="../multiblock/weapons.html">Weapons</a></li>
   </ul>
   <h3 class="nav-title">Guides</h3>
   <ul>
-  //* leaving earth guide
+  <!-- //* leaving earth guide -->
   <li><a id="guides-leaving-earth" href="../guides/leaving-earth.html">Getting off earth</a></li>
-  //* moneymaking guide
+  <!-- //* moneymaking guide -->
   <li><a id="guides-moneymaking" href="../guides/moneymaking.html">Moneymaking</a></li>
   </ul>
 */
