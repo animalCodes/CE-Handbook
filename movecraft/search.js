d03c7c7a909c7520afdce73999a6aa289686823e
animal <no>
2022-03-04 10:30:09 +0000

    finish reformating, redo search function to work on grid

--- movecraft/search.js
+++ movecraft/search.js
@@ -4,18 +4,13 @@ function search(id) {
   const rows = document.getElementById(`${id}-table`).children;
   const input = search.value;
 
-  // for all children of rows, set data to *their* children
-  for (let i = 0; i < rows.length; i++) {
-    let data = rows.item(i).children;
-
-    for (let i = 0; i < data.length; i++) {
-      let val = data.item(i)
-      // if element is a <th>, break.
-      if (val.tagName === "TH") {break;}
+    for (let i = 0; i < rows.length; i++) {
+      let val = rows.item(i)
+      // if element is a <h3> or <input>, break.
+      if (val.tagName === "H3" || val.tagName === "INPUT") {continue}
       // if child doesn't include input, hide
-      if (!val.innerText.includes(input)) { data.item(i).style.display = 'none' }
+      if (!val.innerText.includes(input)) { val.style.display = 'none' }
       // if child does include input, show
-      if (val.innerText.includes(input)) { data.item(i).style.display = 'table-cell' }
+      if (val.innerText.includes(input)) { val.style.display = 'inline-block' }
     }
-  }
 }
